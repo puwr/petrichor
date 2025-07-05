7d788917693bc7d88ffbb0a9f785558f38f3c166
puwr <puwr@proton.me>
2025-07-05 17:25:26 +0500

    refactor(frontend/gallery-page): remove unnecessary manul subscriptions

--- src/Frontend/src/app/pages/gallery-page/gallery-page.component.ts
+++ src/Frontend/src/app/pages/gallery-page/gallery-page.component.ts
@@ -1,14 +1,14 @@
-import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ImageService } from '../../core/services/image.service';
 import { GalleryItem } from '../../shared/models/image';
 import { GalleryComponent } from '../../shared/components/gallery/gallery.component';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { PaginationComponent } from '../../shared/components/pagination/pagination.component';
 import { PagedResponse } from '../../shared/models/pagination';
 import { ActivatedRoute, Router } from '@angular/router';
 import { TextInputComponent } from '../../shared/components/text-input/text-input.component';
-import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
-import { debounceTime, distinctUntilChanged } from 'rxjs';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { distinctUntilChanged, Observable, switchMap, tap } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-gallery-page',
@@ -17,18 +17,16 @@ import { debounceTime, distinctUntilChanged } from 'rxjs';
     PaginationComponent,
     TextInputComponent,
     ReactiveFormsModule,
+    AsyncPipe,
   ],
   templateUrl: './gallery-page.component.html',
   styleUrl: './gallery-page.component.scss',
 })
-export class GalleryPageComponent implements OnInit {
+export class GalleryPageComponent {
   private router = inject(Router);
   private route = inject(ActivatedRoute);
   private fb = inject(FormBuilder);
   private imageService = inject(ImageService);
-  private destroyRef = inject(DestroyRef);
-
-  galleryData: PagedResponse<GalleryItem> | null = null;
 
   searchForm = this.fb.group({
     tags: [''],
@@ -36,24 +34,20 @@ export class GalleryPageComponent implements OnInit {
 
   searchTags: string[] = [];
 
-  ngOnInit(): void {
-    this.route.queryParamMap
-      .pipe(
-        distinctUntilChanged((prev, curr) => {
-          return (
-            prev.get('page') === curr.get('page') &&
-            this.tagsEqual(prev.getAll('tags'), curr.getAll('tags'))
-          );
-        }),
-        takeUntilDestroyed(this.destroyRef)
-      )
-      .subscribe((params) => {
-        const pageNumber = Number(params.get('page')) || 1;
-        this.searchTags = params.getAll('tags');
+  galleryData$ = this.route.queryParamMap.pipe(
+    distinctUntilChanged((prev, curr) => {
+      return (
+        prev.get('page') === curr.get('page') &&
+        this.tagsEqual(prev.getAll('tags'), curr.getAll('tags'))
+      );
+    }),
+    switchMap((params) => {
+      const pageNumber = Number(params.get('page')) || 1;
+      this.searchTags = params.getAll('tags');
 
-        this.loadGalleryData(pageNumber);
-      });
-  }
+      return this.loadGalleryData(pageNumber);
+    })
+  );
 
   onSearch(): void {
     const tags =
@@ -72,20 +66,19 @@ export class GalleryPageComponent implements OnInit {
     }
   }
 
-  private loadGalleryData(pageNumber: number): void {
-    this.imageService
-      .getImages(pageNumber, this.searchTags)
-      .pipe(takeUntilDestroyed(this.destroyRef))
-      .subscribe((response) => {
-        this.galleryData = response;
-
+  private loadGalleryData(
+    pageNumber: number
+  ): Observable<PagedResponse<GalleryItem>> {
+    return this.imageService.getImages(pageNumber, this.searchTags).pipe(
+      tap((response) => {
         if (response.totalPages > 0 && pageNumber > response.totalPages) {
           this.router.navigate([], {
             queryParams: { page: response.totalPages },
             queryParamsHandling: 'merge',
           });
         }
-      });
+      })
+    );
   }
 
   private tagsEqual(a: string[], b: string[]): boolean {
