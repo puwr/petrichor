0fddaf6eca2e4d34a370a3e3670c54c9f0475e96
puwr <puwr@proton.me>
2025-07-30 13:14:11 +0500

    docs: add API reference (OpenAPI + Scalar)

--- src/API/Petrichor.Api/Program.cs
+++ src/API/Petrichor.Api/Program.cs
@@ -1,11 +1,11 @@
 using System.Reflection;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Versioning;
 using Petrichor.Shared.Application;
 using Petrichor.Shared.Infrastructure;
 using Petrichor.Modules.Gallery.Presentation;
 using Petrichor.Modules.Users.Presentation;
 using Petrichor.Api.Extensions;
+using Scalar.AspNetCore;
+using Asp.Versioning;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,11 +14,17 @@ builder.Services.AddProblemDetails();
 
 builder.Services.AddCors();
 
-builder.Services.AddApiVersioning(options =>
+builder.Services
+    .AddApiVersioning(options =>
     {
         options.DefaultApiVersion = new ApiVersion(1, 0);
         options.AssumeDefaultVersionWhenUnspecified = true;
         options.ApiVersionReader = new UrlSegmentApiVersionReader();
+    })
+    .AddApiExplorer(options =>
+    {
+        options.GroupNameFormat = "'v'VVV";
+        options.SubstituteApiVersionInUrl = true;
     });
 
 Assembly[] moduleApplicationAssemblies = [
@@ -34,10 +40,19 @@ builder.Configuration.AddModuleConfiguration(["users"]);
 builder.Services.AddUsersModule(builder.Configuration);
 builder.Services.AddGalleryModule(builder.Configuration);
 
+builder.Services.AddOpenApi();
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
+    app.MapOpenApi();
+    app.MapScalarApiReference(options =>
+    {
+        options.WithTitle("API Reference");
+        options.WithTheme(ScalarTheme.Mars);
+    });
+
     app.ApplyMigrations();
 }
 
